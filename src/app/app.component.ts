4980fc454b75e70c21d0d65c75eda883088eb325
en <huien122@gmail.com>
2023-12-10 21:24:21 +0800

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { MyFirstCompComponent } from './my-first-comp/my-first-comp.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MyFirstCompComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/my-first-comp/my-first-comp.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-my-first-comp',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './my-first-comp.component.html',
+  styleUrl: './my-first-comp.component.scss'
+})
+export class MyFirstCompComponent {
+
+  name: string = '';
+  email: string = '';
+  message: string = '';
+
+}
