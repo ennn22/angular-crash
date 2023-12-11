5496b7d15dbaa31c13b3bb5087ddd093bc013915
en <huien122@gmail.com>
2023-12-11 12:51:23 +0800

    two way binding for input and display.

--- src/app/my-first-comp/my-first-comp.component.ts
+++ src/app/my-first-comp/my-first-comp.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-my-first-comp',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './my-first-comp.component.html',
   styleUrl: './my-first-comp.component.scss'
 })
@@ -13,5 +14,17 @@ export class MyFirstCompComponent {
   name: string = '';
   email: string = '';
   message: string = '';
+  isSubmitted: boolean = false;
+  messages: Array<any> = [];
+
+  onSubmit() {
+    this.isSubmitted = true;
+    this.messages.push({
+      'name': this.name,
+      'email': this.email,
+      'message': this.message
+    });
+    console.log(this.messages);
+  }
 
 }
